b1d82b60c39b944573083baaf0c2cb6b2d01388c
Yuriy <yuriy.sam2002@gmail.com>
2022-08-15 00:15:39 +0200

    20 com

--- files/contact.php
+++ files/contact.php
@@ -1,6 +1,8 @@
 $field_name = $_POST['name'];
 $field_email = $_POST['email'];
-$field_message = $_POST['message'];
+$field_message = $_POST['text'];
+$field_company = $_POST['company'];
+$field_tel = $_POST['tel'];
 
 $mail_to = 'yuriy.sam2002@gmail.com';
 
@@ -8,7 +10,9 @@ $subject = 'Message from a site visitor ' . $field_name;
 
 
 $body_message = 'From: '.$field_name."\n";
-$body_message .= 'E-mail: '.$field_email."\n";
+$body_message = 'Company: '.$field_company."\n";
+$body_message = 'Phone: '.$field_company."\n";
+$body_message .= 'E-mail: '.$field_tel."\n";
 $body_message .= 'Message: '.$field_message;
 
 $headers = "From: $email\r\n";
